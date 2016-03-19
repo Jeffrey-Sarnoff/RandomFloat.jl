5de5e01bad8521fff100c484c0f1fba69aef782e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 00:44:01 -0400

    Update RandomFloat.jl

--- src/RandomFloat.jl
+++ src/RandomFloat.jl
@@ -6,7 +6,9 @@ export randfloat
 
 @vectorize_2arg Any ldexp
 
-function randfloat{T<:AbstractFloat}(sigRange::FloatRange{T},expRange::UnitRange{Int},n::Int)
+function randfloat{T<:AbstractFloat}(sigRange::FloatRange{T}=sqrt(eps(1.0)):1.0,
+                                     expRange::UnitRange{Int}=14:37,
+                                     n::Int=10)
    s = rand(sigRange,n)
    e = rand(expRange,n)
    ldexp(s,e)   
